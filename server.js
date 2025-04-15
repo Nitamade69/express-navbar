4ea6fce6e8689fb11e12ee8b2206d4f25ca0794e
Nitamade69 <brcheung666@gmail.com>
2025-04-14 22:46:03 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+
+
+const app = express()
+const PORT = 3000
+
+
+app.use(morgan('tiny'))
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/typesetting', (req, res) => {
+    res.redirect('https://github.com/Nitamade69/typesetting')
+})
+
+app.get('/climate', (req,res) => {
+    res.redirect('https://github.com/Michelyee132/climate-crisis')
+})
+
+app.get('/algo', (req,res) => {
+    res.redirect('https://github.com/Nitamade69/four-sorting-algorithms')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
